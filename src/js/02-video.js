0da646eb6103282892334ee2dd0e7c7d66444c7d
Denis Serkhel <ripvanfish27@gmail.com>
2022-01-09 16:36:25 +0200

    2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+
+    player.on('timeupdate', throttle(function() {
+          player.getCurrentTime().then(function (sec) {
+      console.log('current-time:', sec);
+      localStorage.setItem('videoplayer-current-time', JSON.stringify(sec));
+    });
+  }, 1000),
+);
+
+
+    const currentTime = JSON.parse(localStorage.getItem('videoplayer-current-time')) || 0;
+player.setCurrentTime(currentTime);
