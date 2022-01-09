1f6e790e562fdff377e9c5a8b0f27ade2d2aa4c9
Denis Serkhel <ripvanfish27@gmail.com>
2022-01-09 13:07:37 +0200

    1я задача сделал

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm"
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+const mainGalleryRef = document.querySelector(".gallery");
+const markupGallery = createImgMarkup(galleryItems);
+
+mainGalleryRef.insertAdjacentHTML('afterbegin', markupGallery);
+
+
+
+ function createImgMarkup(galleryItems) {
+   return galleryItems.map(({ preview, original, description }) => {
+        return `
+      
+ <li> <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      title="${description}"
+    />
+  </a></li>
+`;
+    })
+    .join("");
+}
+
+
+let gallery = new SimpleLightbox(".gallery a");
+gallery.on("show.simplelightbox", () => {
+  captionsData: "title";
+  captionDelay: 250;
+});
